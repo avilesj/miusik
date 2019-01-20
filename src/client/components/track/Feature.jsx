291dd6c0865600c06a58ec68170b9d60e11813fa
Jose Aviles <jlapmail@gmail.com>
2019-01-20 19:09:49 -0400

    Displaying features dynamically on track feature page

--- src/client/components/track/Feature.jsx
+++ src/client/components/track/Feature.jsx
@@ -6,25 +6,32 @@ class Feature extends Component {
         this.toggleFetureHelp = this.toggleFetureHelp.bind(this);
     }
 
-    toggleFetureHelp() {
+    toggleFetureHelp(id) {
+        let featureHelpBox = document.getElementById(id);
+
+        featureHelpBox.hidden = !featureHelpBox.hidden;
+
+        let arrow = featureHelpBox.parentElement.querySelector("i");
+        arrow.classList.toggle("fa-chevron-circle-down");
+        arrow.classList.toggle("fa-chevron-circle-up");
 
     }
 
     render() {
         return (
             <div
-                onClick={this.toggleFetureHelp(props.featureName + '-info')}
+                onClick={() => this.toggleFetureHelp(this.props.featureName + '-info')}
                 className="feature track-info-text">
                 <div className="feature-name-container">
-                    <h1 className="feature-name">{props.featureName}</h1>
+                    <h1 className="feature-name">{this.props.featureName}</h1>
                 </div>
                 <div className="feature-level-container">
                     <div className="feature-level centered-container" style={{ width: " 0%" }}>
-                        <h2>{props.percentage}</h2>
+                        <h2>{this.props.percentage}</h2>
                     </div>
                     <i className="fas fa-chevron-circle-down" />
                 </div>
-                <div id={props.featureName + "-info"} className="feature-help" hidden>
+                <div id={this.props.featureName + "-info"} className="feature-help" hidden>
                     <p>
                         Danceability describes how suitable a track is for dancing based
                         on a combination of musical elements including tempo, rhythm
--- src/client/components/track/TrackFeatures.jsx
+++ src/client/components/track/TrackFeatures.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import Feature from './Feature.jsx';
 
-const TrackFeatures = () => {
+const TrackFeatures = (props) => {
+    console.log("Feature props: ", props.features.data);
+
+    const featureList = ["danceability", "energy", "acousticness", "instrumentalness", "liveness", "speechiness"];
+
+    const featureMap = new Map(Object.entries(props.features.data));
+    const featureComponents = [];
+    featureMap.forEach((value, feature) =>{
+        let featureName = feature;
+        let featureValue = value;
+        console.log("Rendering: ", featureName);
+        featureComponents.push(<Feature key={featureName} featureName = {featureName} percentage={Math.floor(featureValue * 100)}/>);
+    })
+
+    console.log(featureComponents[1]);
     return (<div className="track-features">
-        <Feature featureName = "Danceability" percentage="55"/>
-        <Feature featureName = "energy" percentage="55"/>
+        {featureComponents.filter((component) => featureList.includes(component.props.featureName))};
     </div>);
 }
 
