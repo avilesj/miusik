058da1d87d8535d604b9db101676c681149dcb3b
pavilesjose <jlapmail@gmail.com>
2019-01-25 11:35:47 -0400

    Update pageController.js

--- src/server/controllers/pageController.js
+++ src/server/controllers/pageController.js
@@ -3,8 +3,13 @@ const router = express.Router();
 const path = require('path');
 
 // Home page route.
-router.get('/*', function (req, res) {
+router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'..', '..', 'client', 'views', 'index.html'));
 });
+
+router.get('/track/*', function(req, res) {
+  res.sendFile(path.join(__dirname,'..', '..', 'client', 'views', 'index.html'));
+});
+
 
 module.exports = router;
